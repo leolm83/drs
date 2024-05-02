af13a63f405dac81df6d3f262f8742ce3a61375e
leolm83 <leonardo.mateus01@hotmail.com>
2024-05-02 14:28:52 -0300

    ajuste ler todas as transacoes

--- BankTransactionAnalizer.java
+++ BankTransactionAnalizer.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -5,14 +6,17 @@ import java.util.List;
 
 public class BankTransactionAnalizer {
     private static final String RESOURCES = "src/main/resources";
-    public static void main(String[] args) {
+
+    // read all transactions
+    public static void main(String[] args) throws IOException {
         final Path path = Paths.get(RESOURCES,args[0]);
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
         for(final String line : lines){
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(colums[1]);
+            final double amount = Double.parseDouble(columns[1]);
             total += amount;
-        }
+        } 
+        System.out.println("Total for all transactions is "+total);
     }
 }
