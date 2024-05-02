1e90e8511b7ab46e00ca12aba4f1b0b7f52c8f2b
leolm83 <leonardo.mateus01@hotmail.com>
2024-05-02 14:54:32 -0300

    2-2

--- BankTransactionAnalizer.java
+++ BankTransactionAnalizer.java
@@ -2,21 +2,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalizer {
     private static final String RESOURCES = "src/main/resources";
 
-    // read all transactions
+    // read all transactions in january
     public static void main(String[] args) throws IOException {
         final Path path = Paths.get(RESOURCES,args[0]);
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
         for(final String line : lines){
             final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0],DATE_PATTERN);
+            if(date.getMonth() == Month.JANUARY){
             final double amount = Double.parseDouble(columns[1]);
             total += amount;
+            }
         } 
-        System.out.println("Total for all transactions is "+total);
+        System.out.println("Total for all transactions in january is "+total);
     }
 }
